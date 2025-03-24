790333d6dbedd62f225fdf61580b7855ca15177e
Muhammad-Ayub830 <muhammadayubmrk@gmail.com>
2025-03-24 06:26:55 +0500

    deployed in full version for the first time

--- courses.js
+++ courses.js
@@ -1,3 +1,4 @@
+// logics for toggle btn------------
 var orderList = document.getElementById("orderList");
 console.log(orderList);
 var menu_btn = document.getElementById("toggle_btn")
@@ -10,3 +11,24 @@ menu_btn.addEventListener('click',function(){
     }
    
 })
+// ---------logics for scrolling to top button----------
+let mybutton = document.getElementById("top-btn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+    
+  } else {
+    mybutton.style.display = "none";
+    
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
--- index.js
+++ index.js
@@ -18,20 +18,17 @@ cardItme.forEach(function(card){
         if(cardItme[i]!=card){
            cardItme[i].classList.remove("activeee")
         }else{
-            // img in of close and open 
             if(pic[i].src == plus){
                 pic[i].src = minus
             }else{
                 pic[i].src = plus;
             }
-            // toggling the area of content
             card.classList.toggle("activeee")
         }
     }
-
   })
 })
-// ---------logics for accordain section ended------
+// ---------logics for toggle button------
 var orderList = document.getElementById("orderList");
 console.log(orderList);
 var menu_btn = document.getElementById("toggle_btn")
@@ -44,3 +41,24 @@ menu_btn.addEventListener('click',function(){
     }
    
 })
+// ---------logics for scrolling to top button----------
+let mybutton = document.getElementById("top-btn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+    
+  } else {
+    mybutton.style.display = "none";
+    
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
